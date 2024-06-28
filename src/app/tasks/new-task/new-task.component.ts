f146e1df95fd0e00f89770773938dff46c53c176
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-06-28 10:10:36 +0200

    feat(UI): Use of submitted data

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -10,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
@@ -18,6 +22,8 @@ export class NewTaskComponent {
     this.cancel.emit();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, summary: this.enteredSummary, dueDate: this.enteredDate });
+  }
 
 }
